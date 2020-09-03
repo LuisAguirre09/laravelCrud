2b1b381d12ffda313462a1e0d0d72226eea404b9
eduardo-af <laguirre@quaxar.com>
2020-09-03 15:27:55 -0500

    conflict fixed

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,3 @@ Route::get('/update','crudController@update');
 Route::get('/delete','crudController@delete');
 Route::get('/show','crudController@show');*/
 Route::resource('/crud','crudController');
-
-Route::get('/reed', function(){
-    $products=App\Product::all();
-
-    foreach ($products as $product) {
-        echo $product->nameProduct . "<br>";
-    }
-});
