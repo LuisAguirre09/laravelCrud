d4bee9f0318825d298f414db0a8f3d612fb10a8c
eduardo-af <laguirre@quaxar.com>
2020-09-03 02:47:42 -0500

    reed complete

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,5 +1,8 @@
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" >Crud en laravel</a>
-</nav>   
+<nav class="navbar navbar-light bg-light justify-content-between">
+  <a class="navbar-brand">Crud en Laravel</a>
+  <form class="form-inline">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Agregar Producto</button>
+  </form>
+</nav>
