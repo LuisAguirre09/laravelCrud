8700af0f758e441ac6fbf27b44aace3eeafc91a0
eduardo-af <laguirre@quaxar.com>
2020-09-04 00:14:57 -0500

    conflict fixed

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -29,7 +29,7 @@
         
         <form method="post" action="{{URL::action('crudController@destroy', $pr->id)}}">
             {{csrf_field()}}
-            <a href="#" class="btn btn-primary">Editar</a>
+            <a href="{{URL::action('crudController@edit', $pr->id)}}" class="btn btn-primary">Editar</a>
             <input type="hidden" name="_method" value="DELETE">
             <button type="submit" class="btn btn-outline-danger">
             Eliminar producto
